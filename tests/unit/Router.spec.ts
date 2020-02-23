03534004cef47ed46f3cca8eb60cabd90f74a12a
butterchoco <ahmad.supriyanto@ui.ac.id>
2020-02-23 14:11:11 +0700

    [REFACTOR] Change Unit Test for Routing

--- tests/unit/Router.spec.ts
+++ tests/unit/Router.spec.ts
@@ -1,44 +1,47 @@
-import { shallowMount, mount, createLocalVue } from "@vue/test-utils";
-import App from "@/App.vue";
-import VueRouter from "vue-router";
+import { shallowMount } from "@vue/test-utils";
+import Store from "@/store/index.ts";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Program from "@/views/Program.vue";
 import routes from "@/router/index.ts";
 
-const localVue = createLocalVue();
-localVue.use(VueRouter);
-
 describe("App", () => {
   it("renders Home component via routing", async () => {
     const router = routes;
-    const wrapper = mount(App, {
-      localVue,
-      router
+    const store = Store;
+    const wrapper = shallowMount(Home, {
+      router,
+      store
     });
-    expect(wrapper.find(Home).exists()).toBe(true);
-  });
+    expect(wrapper.vm.$route.path).toBe("/");
+  }, 1000);
 
   it("render About component via routing", async () => {
     const router = routes;
-    const wrapper = mount(App, {
-      localVue,
-      router
+    const store = Store;
+    const wrapper = shallowMount(About, {
+      router,
+      store
     });
-    router.push("/about");
+    router.push("/about").catch(() => {
+      expect(wrapper.vm.$route.path).not.toBe("/about");
+    });
     await wrapper.vm.$nextTick();
-    expect(wrapper.find(About).exists()).toBe(true);
-  });
+    expect(wrapper.vm.$route.path).toBe("/about");
+  }, 1000);
 
   it("render Program component via routing", async () => {
     const router = routes;
-    const wrapper = mount(App, {
-      localVue,
-      router
+    const store = Store;
+    const wrapper = shallowMount(Program, {
+      router,
+      store
     });
 
-    router.push("/program");
+    router.push("/program").catch(() => {
+      expect(wrapper.vm.$route.path).not.toBe("program");
+    });
     await wrapper.vm.$nextTick();
-    expect(wrapper.find(Program).exists()).toBe(true);
-  });
+    expect(wrapper.vm.$route.path).toBe("/program");
+  }, 1000);
 });
