43508858abc793b46229a000645e4cd6bcd12da7
butterchoco <ahmad.supriyanto@ui.ac.id>
2020-02-27 10:30:22 +0700

    [RED] Added NavBeforeLogin Test

---
+++ tests/unit/NavBeforeLogIn.spec.ts
@@ -0,0 +1,60 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import NavBeforeLogin from "@/components/NavBeforeLogin.vue";
+import App from "@/App.vue";
+import VueRouter from "vue-router";
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
+describe("NavBeforeLogin Data", () => {
+  let wrapper = shallowMount(NavBeforeLogin, {
+    localVue
+  });
+
+  it("has isScrolled data", () => {
+    expect(wrapper.vm.$data.isScrolled).toBeDefined();
+  });
+
+  it("has isDropdownActive data", () => {
+    expect(wrapper.vm.$data.isDropdownActive).toBeDefined();
+  });
+
+  it("has mobile data", () => {
+    expect(wrapper.vm.$data.mobile).toBeDefined();
+  });
+
+  it("change isDropdownActive if dropdownActive function called", () => {
+    expect(wrapper.vm.$data.isDropdownActive).toBe(true);
+    wrapper.find(".mob-btn").trigger("click");
+    expect(wrapper.vm.$data.isDropdownActive).toBe(false);
+  });
+
+  it("calls windowToggle function", () => {
+    const windowToggle = jest.fn();
+    wrapper = shallowMount(NavBeforeLogin, {
+      localVue,
+      methods: {
+        windowToggle
+      }
+    });
+    wrapper.vm.$nextTick();
+    expect(windowToggle).toBeCalled();
+  });
+
+  it("calls styleNavbarWhenScrolled function", () => {
+    const styleNavbarWhenScrolled = jest.fn();
+    wrapper = shallowMount(NavBeforeLogin, {
+      localVue,
+      data() {
+        return {
+          isScrolled: false
+        };
+      },
+      methods: {
+        styleNavbarWhenScrolled
+      }
+    });
+    wrapper.vm.$nextTick();
+    expect(styleNavbarWhenScrolled).toBeCalled();
+  });
+});
