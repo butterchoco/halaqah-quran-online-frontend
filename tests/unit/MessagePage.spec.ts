f3542252ef8caba820497d8a7ab83d2da278b5aa
Muhammad Farras hakim <farrashakim@gmail.com>
2020-02-29 13:57:53 +0700

    [RED]Added unit tset for message page component

---
+++ tests/unit/MessagePage.spec.ts
@@ -0,0 +1,30 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import MessagePage from "@/components/MessagePage.vue";
+import VueRouter from "vue-router";
+
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+describe("MessagePage Data", () => {
+  let wrapper = shallowMount(MessagePage, {
+    localVue
+  });
+  it("renders data when passed", () => {
+    const prop = {
+      msg: "this is a message",
+      title: "this is a title",
+      img: require("@/assets/img/success-selection-1.png"),
+      goto: [
+        {
+          link: "/",
+          str: "Pilih Jadwal"
+        }
+      ]
+    };
+    const wrapper = shallowMount(MessagePage, {
+      propsData: prop,
+      localVue,
+      stubs: ["router-link", "router-view"]
+    });
+    expect(wrapper.props()).toMatchObject(prop);
+  });
+});
