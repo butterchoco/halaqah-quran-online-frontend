19afb8fd2e2c921f0e7588e4006fea1806e4fea1
Muhammad Farras Hakim <muhammad.farras71@ui.ac.id>
2020-03-03 12:49:58 +0700

    [GREEN] Implemented data calon peserta page

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import axios from 'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "@/styles/_global.scss";
+import "@/styles/_variables.scss";
+import Axios from "axios";
 
-Vue.prototype.$axios = axios
-Vue.use(BootstrapVue)
-Vue.config.productionTip = false
+Vue.prototype.$axios = Axios;
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
+
+Vue.prototype.$http = Axios;
+const token = localStorage.getItem("token");
+if (token) {
+  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+}
 
 new Vue({
   router,
